cb6325ac52568113e765324d0211ab992bf9d033
NaritaShosei <onigirikun7171@gmail.com>
2025-02-20 19:21:04 +0900

    [add]Playerの移動

---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using SymphonyFrameWork.CoreSystem;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _moveSpeed;
+    Rigidbody _rb;
+    InputBuffer _input;
+    Vector2 _velocity;
+    void Start()
+    {
+        _input = ServiceLocator.GetInstance<InputBuffer>();
+        _rb = GetComponent<Rigidbody>();
+        AddAction();
+    }
+
+    void Update()
+    {
+        _rb.linearVelocity = new Vector3(_velocity.x, 0, _velocity.y) * _moveSpeed;
+    }
+
+    void Move(InputAction.CallbackContext context)
+    {
+        var input = context.ReadValue<Vector2>();
+        if (context.phase == InputActionPhase.Performed)
+        {
+            _velocity += input;
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            _velocity = Vector2.zero;
+        }
+        if (_velocity.magnitude > 1)
+        {
+            _velocity = _velocity.normalized;
+        }
+    }
+    void Jump(InputAction.CallbackContext context)
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        RemoveAction();
+    }
+
+    void AddAction()
+    {
+        _input.MoveAction.performed += Move;
+        _input.MoveAction.canceled += Move;
+        _input.JumpAction.performed += Jump;
+    }
+
+    void RemoveAction()
+    {
+        _input.MoveAction.performed -= Move;
+        _input.MoveAction.canceled -= Move;
+        _input.JumpAction.performed -= Jump;
+    }
+}
