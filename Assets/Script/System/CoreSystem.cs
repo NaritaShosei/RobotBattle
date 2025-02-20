7d1b5283e21936e1d9c7f91f7db41e76aa663670
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-20 17:03:54 +0900

    [clean]コメント追加

--- Assets/Script/System/CoreSystem.cs
+++ Assets/Script/System/CoreSystem.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 
 public class CoreSystem : MonoBehaviour
 {
+    /// <summary>
+    /// ゲーム開始時にシステムシーンをロード
+    /// </summary>
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void InitializeGame()
     {
