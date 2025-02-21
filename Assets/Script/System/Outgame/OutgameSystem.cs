32dfb76a3798342ec1d9802a0a089b266e9eab9e
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-22 01:49:03 +0900

    [update]アウトゲームからインゲームへの遷移を実装

--- Assets/Script/System/Outgame/OutgameSystem.cs
+++ Assets/Script/System/Outgame/OutgameSystem.cs
@@ -1,6 +1,15 @@
+using SymphonyFrameWork.System;
 using UnityEngine;
 
 public class OutgameSystem : MonoBehaviour
 {
-    
+    /// <summary>
+    /// インゲームをロードする
+    /// </summary>
+    [ContextMenu("StartIngame")]
+    public void StartIngame()
+    {
+        var changer = ServiceLocator.GetInstance<SceneChanger>();
+        changer.ChangeScene(SceneListEnum.Ingame);
+    }
 }
