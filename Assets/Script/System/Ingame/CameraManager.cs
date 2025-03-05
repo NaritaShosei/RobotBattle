4ba2d2af81923f350f0ad2d6830e97189b9768de
NaritaShosei <onigirikun7171@gmail.com>
2025-03-05 16:59:26 +0900

    [update]カメラの反転

--- Assets/Script/System/Ingame/CameraManager.cs
+++ Assets/Script/System/Ingame/CameraManager.cs
@@ -8,7 +8,13 @@ public class CameraManager : MonoBehaviour
     [SerializeField]
     CinemachineOrbitalFollow _freeLookCamera;
     [SerializeField, Range(0.1f, 5)]
-    float _cameraSensitivity = 2;
+    float _cameraSensitivityX = 2;
+    [SerializeField]
+    bool _invertX;
+    [SerializeField, Range(0.1f, 5)]
+    float _cameraSensitivityY = 2;
+    [SerializeField]
+    bool _invertY;
     InputBuffer _input;
     Vector2 _velocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,9 +41,12 @@ public class CameraManager : MonoBehaviour
         {
             _velocity = _velocity.normalized;
         }
-        _freeLookCamera.HorizontalAxis.Value += _velocity.x * _cameraSensitivity;
+        _freeLookCamera.HorizontalAxis.Value += _velocity.x
+            * (_invertX ? _cameraSensitivityX : -_cameraSensitivityX);
+
         _freeLookCamera.VerticalAxis.Value = Mathf.Clamp(
-        _freeLookCamera.VerticalAxis.Value += _velocity.y * _cameraSensitivity,
+        _freeLookCamera.VerticalAxis.Value += _velocity.y
+        * (_invertY ? _cameraSensitivityY : -_cameraSensitivityY),
         _freeLookCamera.VerticalAxis.Range.x,//Rangの範囲内に固定する
         _freeLookCamera.VerticalAxis.Range.y);
     }
