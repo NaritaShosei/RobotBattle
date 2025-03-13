6175fda54e988cafc352d28927f95986c2808493
NaritaShosei <onigirikun7171@gmail.com>
2025-03-13 18:41:45 +0900

    [add]照準を合わせるscriptの作成

---
+++ Assets/Script/Player/LockOn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOn : MonoBehaviour
+{
+    List<(GameObject obj, Enemy_B enemy)> _enemyList = new();
+    [SerializeField]
+    float _sightAngle;
+    [SerializeField]
+    float _maxDistance;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
