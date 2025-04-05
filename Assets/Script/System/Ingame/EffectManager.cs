2abbf6bd9824c93ffbde565d6500d9024ebc86e2
NaritaShosei <onigirikun7171@gmail.com>
2025-04-05 19:04:28 +0900

    [add]EffectManager

---
+++ Assets/Script/System/Ingame/EffectManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    [SerializeField] Transform _parent;
+
+    [SerializeField] ParticleSystem _explosion;
+    public static EffectManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+
+    }
+
+    public void PlayExplosion(Vector3 pos)
+    {
+        Instantiate(_explosion, pos, Quaternion.identity, _parent);
+    }
+}
