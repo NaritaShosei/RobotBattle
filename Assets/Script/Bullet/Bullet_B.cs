477ea7daced948cffd5fc3db2e208e64ea7bdf89
NaritaShosei <onigirikun7171@gmail.com>
2025-04-05 16:34:34 +0900

    [update]たまっを消す処理

--- Assets/Script/Bullet/Bullet_B.cs
+++ Assets/Script/Bullet/Bullet_B.cs
@@ -1,9 +1,12 @@
 ﻿using Script.System.Ingame;
+using System;
 using UnityEngine;
 
 public class Bullet_B : MonoBehaviour
 {
     Vector3 _moveDirection;
+    public Action<Bullet_B> ReturnPoolEvent;
+    float _timer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,12 +17,19 @@ public class Bullet_B : MonoBehaviour
     void Update()
     {
         transform.position += _moveDirection * 100 * Time.deltaTime;
+        if (_timer + 10 < Time.time)
+        {
+            gameObject.SetActive(false);
+            ReturnPoolEvent?.Invoke(this);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (TryGetComponent(out IFightable component))
+        if (other.TryGetComponent(out IFightable component))
         {
             AddDamage(0, component);
+            gameObject.SetActive(false);
+            ReturnPoolEvent?.Invoke(this);
         }
     }
 
@@ -30,6 +40,7 @@ public class Bullet_B : MonoBehaviour
     public virtual void SetDirection(Vector3 dir)
     {
         _moveDirection = dir;
+        _timer = Time.time;
     }
     public virtual void SetPosition(Vector3 pos)
     {
