9b5d56fc3a5ab91a0ae753b59526de184b9d62e8
NaritaShosei <onigirikun7171@gmail.com>
2025-04-08 17:44:17 +0900

    [fix]Seqenceに変更　　　　

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -27,7 +27,7 @@ public class PlayerController : Character_B<CharacterData_B>
     float _currentSpeed;
     bool _isJumped;
     bool _isDashed;
-    TweenerCore<float, float, FloatOptions> _dashTween = null;
+    Sequence _dashSeq;
     void Start()
     {
         _input = ServiceLocator.GetInstance<InputBuffer>();
@@ -95,14 +95,14 @@ public class PlayerController : Character_B<CharacterData_B>
             _isDashed = true;
             var vel = _velocity != Vector2.zero ? _moveDir : _camForward;
             _rb.AddForce(new Vector3(vel.x, 0, vel.z) * _dashSpeed * 3, ForceMode.Impulse);
-            _dashTween?.Kill();
-            _dashTween = DOTween.To(() => _currentSpeed, speed => _currentSpeed = speed, _dashSpeed, _dashTime);
+            _dashSeq?.Kill();
+            _dashSeq = DOTween.Sequence(DOTween.To(() => _currentSpeed, speed => _currentSpeed = speed, _dashSpeed, _dashTime));
             StartCoroutine(Dash());
         }
         else if (context.phase == InputActionPhase.Canceled)
         {
-            _dashTween?.Kill();
-            _dashTween = DOTween.To(() => _currentSpeed, x => _currentSpeed = x, _normalSpeed, 0.8f);
+            _dashSeq?.Kill();
+            _dashSeq = DOTween.Sequence(DOTween.To(() => _currentSpeed, x => _currentSpeed = x, _normalSpeed, 0.8f));
         }
 
     }
