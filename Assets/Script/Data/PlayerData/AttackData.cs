49744c019a1974d0faf865df07b2630f9d2194b6
NaritaShosei <onigirikun7171@gmail.com>
2025-04-08 17:44:33 +0900

    [update]Data

--- Assets/Script/Data/PlayerData/AttackData.cs
+++ Assets/Script/Data/PlayerData/AttackData.cs
@@ -3,7 +3,10 @@
 [CreateAssetMenu(fileName = "AttackData", menuName = "GameData")]
 public class AttackData : ScriptableObject
 {
-    public float AttackRate { get => _attackRate; }
+    public float AttackRat => _attackRate;
     [SerializeField]
     float _attackRate = 1f;
+    public float AttackPower => _attackPower;
+    [SerializeField]
+    float _attackPower = 1f;
 }
