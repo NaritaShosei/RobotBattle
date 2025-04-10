43f0514d649374fcceeb17ac6f402004a8158135
NaritaShosei <onigirikun7171@gmail.com>
2025-04-10 19:45:45 +0900

    [add]簡単なロックオン機能

--- Assets/Script/Player/LockOn.cs
+++ Assets/Script/Player/LockOn.cs
@@ -1,20 +1,51 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LockOn : MonoBehaviour
 {
-    List<(GameObject obj, Enemy_B enemy)> _enemyList = new();
     [SerializeField]
-    float _sightAngle;
+    Transform _player;
+
+    [SerializeField]
+    Transform _lockOnCenter;
+
     [SerializeField]
     float _maxDistance;
+
+
+    Enemy_B _lockOnEnemy;
+    List<Enemy_B> _enemies;
     void Start()
     {
-
+        _enemies = FindObjectsByType<Enemy_B>(FindObjectsSortMode.None).ToList();
     }
 
     void Update()
     {
+        Vector3 lockOnCenterScreenPos = Camera.main.WorldToViewportPoint(_lockOnCenter.position);
+        float minDistance = float.MaxValue;
+        _lockOnEnemy = null;
 
+        foreach (var enemy in _enemies)
+        {
+            Vector3 viewPos = Camera.main.WorldToViewportPoint(enemy.transform.position);
+
+            if (viewPos.z < lockOnCenterScreenPos.y) continue;
+
+            Vector2 targetScreenPos = new Vector2(viewPos.x, viewPos.y);
+
+            var dis = Vector2.Distance(lockOnCenterScreenPos, targetScreenPos);
+
+            if (dis < minDistance && dis <= 0.5f)
+            {
+                minDistance = dis;
+                _lockOnEnemy = enemy;
+            }
+        }
+    }
+    public Enemy_B GetTarget()
+    {
+        return _lockOnEnemy;
     }
 }
