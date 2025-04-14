a18599138b734d9e11b6deb60b9c2da366cba094
NaritaShosei <onigirikun7171@gmail.com>
2025-04-14 18:57:03 +0900

    [update]視界内の敵をロックオン

--- Assets/Script/Player/LockOn.cs
+++ Assets/Script/Player/LockOn.cs
@@ -13,6 +13,9 @@ public class LockOn : MonoBehaviour
     [SerializeField]
     float _maxDistance;
 
+    [SerializeField, Range(0, 180)]
+    float _viewAngle = 120;
+
 
     Enemy_B _lockOnEnemy;
     List<Enemy_B> _enemies;
@@ -29,28 +32,28 @@ public class LockOn : MonoBehaviour
 
         foreach (var enemy in _enemies)
         {
-            Vector3 viewPos = Camera.main.WorldToViewportPoint(enemy.transform.position);
+            Vector3 enemyDir = enemy.transform.position - _player.position;
+            Vector3 enemyDirFlat = new Vector3(enemyDir.x, 0, enemyDir.z).normalized;
+            Vector3 playerForward = new Vector3(_player.forward.x, 0, _player.forward.z).normalized;
 
-            if (viewPos.z < lockOnCenterScreenPos.y) continue;
+            float dot = Vector3.Dot(playerForward, enemyDirFlat);
+            float cosViewAngle = Mathf.Cos(_viewAngle * 0.5f * Mathf.Deg2Rad);
+
+            if (dot < cosViewAngle) continue;
+
+            Vector3 viewPos = Camera.main.WorldToViewportPoint(enemy.transform.position);
+            if (viewPos.z < 0f) continue;
 
             Vector2 targetScreenPos = new Vector2(viewPos.x, viewPos.y);
+            float screenDis = Vector2.Distance(lockOnCenterScreenPos, targetScreenPos);
 
-            var screenDis = Vector2.Distance(lockOnCenterScreenPos, targetScreenPos);
 
-            //Debug用にここで実行しているが下のカッコの中でやるようにしたい
-            var dis = enemy.transform.position - _player.position;
-            dis.y = 0;
-            Debug.Log(dis.magnitude);
-            //画面内にいるかつ基準座標からの距離が一番近い敵を保存
-            if (screenDis < minDistance && screenDis <= 0.5f)
+            enemyDir.y = 0;
+
+            if (screenDis < minDistance && screenDis <= 0.5f && enemyDir.magnitude <= _maxDistance)
             {
-                //var dis = enemy.transform.position - _player.position;
-                //dis.y = 0; 
-                if (dis.magnitude <= _maxDistance)
-                {
-                    minDistance = screenDis;
-                    _lockOnEnemy = enemy;
-                }
+                minDistance = screenDis;
+                _lockOnEnemy = enemy;
             }
         }
     }
