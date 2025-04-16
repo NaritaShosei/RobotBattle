95afe398244a422624535c780c2f501694e19ff0
NaritaShosei <onigirikun7171@gmail.com>
2025-04-16 16:59:41 +0900

    [update]ロックオン処理を視界に変更

--- Assets/Script/Player/LockOn.cs
+++ Assets/Script/Player/LockOn.cs
@@ -14,8 +14,10 @@ public class LockOn : MonoBehaviour
     float _maxDistance;
 
     [SerializeField, Range(0, 180)]
-    float _viewAngle = 120;
+    float _viewAngleHorizontal = 120;
 
+    [SerializeField, Range(0, 180)]
+    float _viewAngleVertical = 60;
 
     Enemy_B _lockOnEnemy;
     List<Enemy_B> _enemies;
@@ -33,24 +35,40 @@ public class LockOn : MonoBehaviour
         foreach (var enemy in _enemies)
         {
             Vector3 enemyDir = enemy.transform.position - _player.position;
-            Vector3 enemyDirFlat = new Vector3(enemyDir.x, 0, enemyDir.z).normalized;
-            Vector3 playerForward = new Vector3(_player.forward.x, 0, _player.forward.z).normalized;
 
-            float dot = Vector3.Dot(playerForward, enemyDirFlat);
-            float cosViewAngle = Mathf.Cos(_viewAngle * 0.5f * Mathf.Deg2Rad);
+            var toEnemyDir = enemyDir.normalized;
+            var playerForward = _player.transform.forward.normalized;
 
-            if (dot < cosViewAngle) continue;
+            enemyDir.y = 0;
+            if (enemyDir.magnitude > _maxDistance) continue;
+
+
 
             Vector3 viewPos = Camera.main.WorldToViewportPoint(enemy.transform.position);
             if (viewPos.z < 0f) continue;
 
+            Vector3 enemyDirFlat = new Vector3(toEnemyDir.x, 0, toEnemyDir.z).normalized;
+            Vector3 playerFlatForward = new Vector3(playerForward.x, 0, playerForward.z).normalized;
+
+            float dotHorizontal = Vector3.Dot(playerFlatForward, enemyDirFlat);
+
+            if (dotHorizontal < Mathf.Cos(_viewAngleHorizontal * 0.5f * Mathf.Deg2Rad)) continue;
+
+            float verticalDis = new Vector3(toEnemyDir.x, 0, toEnemyDir.z).magnitude;
+            Vector3 verticalEnemy = new Vector3(0, toEnemyDir.y, verticalDis).normalized;
+
+            float forwardFlatDis = new Vector3(playerForward.x, 0, playerForward.z).magnitude;
+            var verticalForward = new Vector3(0, playerForward.y, forwardFlatDis).normalized;
+
+            float dotVertical = Vector3.Dot(verticalForward, verticalEnemy);
+            if (dotVertical < Mathf.Cos(_viewAngleVertical * 0.5f * Mathf.Deg2Rad)) continue;
+
             Vector2 targetScreenPos = new Vector2(viewPos.x, viewPos.y);
             float screenDis = Vector2.Distance(lockOnCenterScreenPos, targetScreenPos);
 
-
             enemyDir.y = 0;
 
-            if (screenDis < minDistance && screenDis <= 0.5f && enemyDir.magnitude <= _maxDistance)
+            if (screenDis < minDistance)
             {
                 minDistance = screenDis;
                 _lockOnEnemy = enemy;
