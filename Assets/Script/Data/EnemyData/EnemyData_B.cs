5141d167e6d5e31e7822bc6eda2b491dcd1913bb
NaritaShosei <onigirikun7171@gmail.com>
2025-04-19 02:59:06 +0900

    [add]Enemy用のData

---
+++ Assets/Script/Data/EnemyData/EnemyData_B.cs
@@ -0,0 +1,13 @@
+﻿using Script.System.Ingame;
+using System;
+using UnityEngine;
+
+public class EnemyData_B : CharacterData_B
+{
+    [SerializeField]
+    float _dodgeTime = 5;
+    public float DodgeTime => _dodgeTime;
+    [NonSerialized]
+    public float DodgeTimer;
+
+}
