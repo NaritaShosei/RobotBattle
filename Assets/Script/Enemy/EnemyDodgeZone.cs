9b8dc0fe9c981762040acde0e67e49a11b0c2542
NaritaShosei <onigirikun7171@gmail.com>
2025-04-18 17:29:08 +0900

    [add]回避判定用のコード

---
+++ Assets/Script/Enemy/EnemyDodgeZone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyDodgeZone : MonoBehaviour
+{
+    [SerializeField]
+    Collider _collider;
+    public Collider Collider => _collider;
+
+    bool _isDodge;
+    public bool IsDodge => _isDodge;
+
+    public Action<Collider> OnTriggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTriggerEnterEvent?.Invoke(other);
+    }
+}
