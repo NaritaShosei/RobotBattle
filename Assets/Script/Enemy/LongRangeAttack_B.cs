0c8d066979704614f9f6d9b7b48ca1852dac1e8a
NaritaShosei <onigirikun7171@gmail.com>
2025-04-22 14:55:39 +0900

    [add]遠距離攻撃の基底

---
+++ Assets/Script/Enemy/LongRangeAttack_B.cs
@@ -0,0 +1,49 @@
+﻿using SymphonyFrameWork.System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongRangeAttack_B : MonoBehaviour
+{
+    [SerializeField] protected AttackData _data;
+
+    [SerializeField] protected Bullet_B _bullet;
+
+    [SerializeField] protected Transform _muzzle;
+
+    [SerializeField] protected Transform _bulletParent;
+
+    [SerializeField] protected LayerMask _layer;
+    protected Queue<Bullet_B> _bulletPool = new();
+    protected bool _isAttacked;
+    protected float _time;
+    protected int _count;
+
+    protected void Start_B()
+    {
+        OnStart();
+    }
+
+    protected void Update_B()
+    {
+
+    }
+
+    protected virtual void OnStart()
+    {
+        _time = Time.time;
+        _count = _data.BulletCount;
+        for (int i = 0; i < _data.BulletCount; i++)
+        {
+            var bullet = Instantiate(_bullet, _bulletParent);
+            bullet.ReturnPoolEvent = OnReturnPool;
+            bullet.gameObject.SetActive(false);
+            bullet.SetAttackValue(_data.AttackPower);
+            _bulletPool.Enqueue(bullet);
+            bullet.gameObject.layer = _layer;
+        }
+    }
+    void OnReturnPool(Bullet_B bullet)
+    {
+        _bulletPool.Enqueue(bullet);
+    }
+}
