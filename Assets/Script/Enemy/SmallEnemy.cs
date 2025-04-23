68a26a9f805cf92fb51fd8ac82e312070fe17fe7
NaritaShosei <onigirikun7171@gmail.com>
2025-04-23 19:51:33 +0900

    [update]雑魚敵の攻撃範囲

--- Assets/Script/Enemy/SmallEnemy.cs
+++ Assets/Script/Enemy/SmallEnemy.cs
@@ -2,15 +2,37 @@
 
 public class SmallEnemy : Enemy_B
 {
-    [SerializeField]
+    [SerializeField] SmallEnemyTargetZone _collider;
+
     void Start()
     {
         OnStart();
         Initialize(_dataBase);
+        _collider.OnTriggerEnterEvent += TargetSet;
+        _collider.OnTriggerExitEvent += TargetUnset;
     }
 
     void Update()
     {
-        OnAttackEvent?.Invoke(_player);
+        if (_player)
+        {
+            OnAttackEvent?.Invoke(_player);
+        }
+    }
+
+    void TargetSet(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerController player))
+        {
+            _player = player;
+        }
+    }
+
+    void TargetUnset(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerController player))
+        {
+            _player = null;
+        }
     }
 }
