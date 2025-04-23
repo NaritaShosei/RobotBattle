685358dfa91d3d545b7174b7d131eb62a5fc3a13
NaritaShosei <onigirikun7171@gmail.com>
2025-04-23 16:07:51 +0900

    [fix]Enemyを大幅修正

---
+++ Assets/Script/Enemy/EnemyLongRangeAttack.cs
@@ -0,0 +1,38 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class EnemyLongRangeAttack : LongRangeAttack_B
+{
+    [SerializeField]
+    void Start()
+    {
+        Start_B();
+    }
+
+    void Update()
+    {
+        if (_isAttacked)
+        {
+            if (_bulletPool.Count != 0 && _count != 0)
+            {
+                float rate = 1 / _data.AttackRate;
+                if (Time.time > _time + rate)
+                {
+                    _time = Time.time;
+                    var bullet = _bulletPool.Dequeue();
+                    bullet.SetPosition(_muzzle.position);
+
+                    bullet.SetDirection(transform.forward);
+                    bullet.SetTarget(enemy);
+                    bullet.gameObject.SetActive(true);
+                    _count--;
+                }
+            }
+        }
+    }
+    async UniTaskVoid Reload()
+    {
+        await UniTask.Delay((int)(_data.ReloadInterval * 1000));
+        _count = _data.BulletCount;
+    }
+}
---
+++ Assets/Script/Enemy/SmallEnemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SmallEnemy : Enemy_B
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
