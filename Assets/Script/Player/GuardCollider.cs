245f66f0d9458ddf65d76ef6d362f698c9e5e79a
NaritaShosei <onigirikun7171@gmail.com>
2025-05-01 03:40:30 +0900

    [add]Playerのガードのコライダー

---
+++ Assets/Script/Player/GuardCollider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class GuardCollider : MonoBehaviour
+{
+    [SerializeField]
+    Collider _collider;
+    public Collider Collider => _collider;
+
+    public Action<Collider> OnTriggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTriggerEnterEvent?.Invoke(other);
+    }
+}
