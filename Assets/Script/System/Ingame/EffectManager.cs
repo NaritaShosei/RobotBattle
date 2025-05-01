5e3818a834d84be622711d162e3f56b16b1e1952
NaritaShosei <onigirikun7171@gmail.com>
2025-05-02 01:59:21 +0900

    [update]EffectManagerをオブジェクトプールに変更

--- Assets/Script/System/Ingame/EffectManager.cs
+++ Assets/Script/System/Ingame/EffectManager.cs
@@ -6,6 +6,9 @@ public class EffectManager : MonoBehaviour
     [SerializeField] Transform _parent;
 
     [SerializeField] ParticleSystem _explosion;
+
+    Queue<ParticleSystem> _pool = new();
+    List<ParticleSystem> _particles = new();
     public static EffectManager Instance { get; private set; }
 
     private void Awake()
@@ -15,11 +18,37 @@ public class EffectManager : MonoBehaviour
 
     private void Start()
     {
-
+        for (int i = 0; i < 50; i++)
+        {
+            var effect = Instantiate(_explosion, _parent);
+            effect.gameObject.SetActive(false);
+            _pool.Enqueue(effect);
+        }
     }
 
     public void PlayExplosion(Vector3 pos)
     {
-        Instantiate(_explosion, pos, Quaternion.identity, _parent);
+        if (_pool.Count == 0)
+        {
+            Debug.LogWarning("プールが空になりました");
+            var newEffect = Instantiate(_explosion, _parent);
+            newEffect.gameObject.SetActive(false);
+            _pool.Enqueue(newEffect);
+        }
+
+        var effect = _pool.Dequeue();
+        effect.transform.position = pos;
+        effect.gameObject.SetActive(true);
+        _particles.Add(effect);
+
+
+        for (int i = _particles.Count - 1; i >= 0; i--)
+        {
+            if (_particles[i].gameObject.activeSelf == false)
+            {
+                _pool.Enqueue(_particles[i]);
+                _particles.RemoveAt(i);
+            }
+        }
     }
 }
