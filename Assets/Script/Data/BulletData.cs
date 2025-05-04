04b189d0245b3348d23153c545956563d2cfdba5
NaritaShosei <onigirikun7171@gmail.com>
2025-05-04 16:11:15 +0900

    [add]弾のData

---
+++ Assets/Script/Data/BulletData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "BulletData",fileName = "BulletData")]
+public class BulletData : ScriptableObject
+{
+    public float AttackPower => _attackPower;
+    [SerializeField]
+    float _attackPower = 1f;
+
+    public float BreakPoint => _breakPoint;
+    [SerializeField]
+    float _breakPoint;
+}
