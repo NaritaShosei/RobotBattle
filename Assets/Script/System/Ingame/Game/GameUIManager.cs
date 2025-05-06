e3434e91dce0572278febd8a1231d85cdbbb204e
NaritaShosei <onigirikun7171@gmail.com>
2025-05-07 02:38:42 +0900

    [add]UIのマネージャークラス

---
+++ Assets/Script/System/Ingame/Game/GameUIManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameUIManager : MonoBehaviour
+{
+    public static GameUIManager Instance { get; private set; }
+
+    [SerializeField] TimeView _timeView;
+    public TimeView TimeView => _timeView;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
