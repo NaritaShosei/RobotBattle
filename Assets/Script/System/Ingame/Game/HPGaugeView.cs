dae0001064586f010d64e295d67e3349c79c39db
NaritaShosei <onigirikun7171@gmail.com>
2025-05-08 14:15:32 +0900

    [add]HPゲージのView

---
+++ Assets/Script/System/Ingame/Game/HPGaugeView.cs
@@ -0,0 +1,33 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPGaugeView : MonoBehaviour
+{
+    [SerializeField]
+    Image _backGauge;
+    [SerializeField]
+    Image _gauge;
+    Sequence _sequence;
+
+    float _maxHealth;
+
+    public void Initialize(float maxHealth)
+    {
+        _maxHealth = maxHealth;
+    }
+
+    public void GaugeUpdate(float currentValue)
+    {
+        float value = currentValue / _maxHealth;
+        _gauge.fillAmount = value;
+
+        _sequence?.Kill();
+
+        //Enemyにも使えるようにSetLinkを使う
+        _sequence = DOTween.Sequence().
+                    SetLink(gameObject).
+                    SetDelay(0.75f).
+                    Append(_backGauge.DOFillAmount(value, 0.5f));
+    }
+}
