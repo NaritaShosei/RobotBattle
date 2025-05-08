08bc21bfdbe2159f1e99f11796375817ea12147d
NaritaShosei <onigirikun7171@gmail.com>
2025-05-08 14:15:47 +0900

    [add]HPゲージのPresenter

---
+++ Assets/Script/System/Ingame/Game/HPGaugePresenter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HPGaugePresenter
+{
+    HPGaugeView _view;
+
+    public HPGaugePresenter(HPGaugeView view)
+    {
+        _view = view;
+    }
+
+    public void Initialize(float maxHealth)
+    {
+        _view.Initialize(maxHealth);
+    }
+
+    public void GaugeUpdate(float currentHealth)
+    {
+        _view?.GaugeUpdate(currentHealth);
+    }
+}
