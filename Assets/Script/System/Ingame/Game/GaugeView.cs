3dea266124fc30047e0407bdf27aea176fb987df
NaritaShosei <onigirikun7171@gmail.com>
2025-05-08 14:58:34 +0900

    [add]スタミナのようなゲージのView

---
+++ Assets/Script/System/Ingame/Game/GaugeView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class GaugeView : MonoBehaviour
+{
+    [SerializeField]
+    Image _backGauge;
+    [SerializeField]
+    Image _gauge;
+    Sequence _sequence;
+
+    float _maxGauge;
+
+    public void Initialize(float maxGauge)
+    {
+        _maxGauge = maxGauge;
+    }
+
+    public void GaugeUpdate(float currentValue)
+    {
+        float value = currentValue / _maxGauge;
+        _gauge.fillAmount = value;
+
+        _sequence?.Kill();
+
+        _sequence = DOTween.Sequence().
+                    Append(_backGauge.DOFillAmount(value, 0.5f));
+    }
+}
