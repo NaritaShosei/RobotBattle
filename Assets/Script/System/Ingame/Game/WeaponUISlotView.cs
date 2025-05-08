9789daceee0938fbb0b0555bbce081699d9e7e7e
NaritaShosei <onigirikun7171@gmail.com>
2025-05-08 10:29:16 +0900

    [update]最大弾数の表示

--- Assets/Script/System/Ingame/Game/WeaponUISlotView.cs
+++ Assets/Script/System/Ingame/Game/WeaponUISlotView.cs
@@ -11,19 +11,21 @@ public class WeaponUISlotView : MonoBehaviour
     RectTransform _rectTransform;
     [SerializeField]
     CanvasGroup _canvasGroup;
-
+    int _maxCount;
     public void SetContent(int count, Sprite icon)
     {
         _iconImage.sprite = icon;
-        _bulletCountText.text = count.ToString("000");
+        _maxCount = count;
+        _bulletCountText.text = $"{count:000}/{_maxCount:000}";
     }
 
     public void SetCount(int count)
     {
-        _bulletCountText.text = count.ToString("000");
+        _bulletCountText.text = $"{count:000}/{_maxCount:000}";
     }
     public void AnimateToFront()
     {
+        _bulletCountText.enabled = true;
         _rectTransform.DOAnchorPosX(100, 0.5f);
         _rectTransform.DOScale(Vector3.one * 5, 0.5f);
         _canvasGroup.DOFade(1, 0.5f);
@@ -32,6 +34,7 @@ public class WeaponUISlotView : MonoBehaviour
 
     public void AnimateToBack()
     {
+        _bulletCountText.enabled = false;
         _rectTransform.DOAnchorPosX(-100, 0.5f);
         _rectTransform.DOScale(Vector3.one * 3, 0.5f);
         _canvasGroup.DOFade(0.5f, 0.5f);
