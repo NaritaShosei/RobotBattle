b268c8d68a7c8fe12a1b366534e344a184e3203f
NaritaShosei <onigirikun7171@gmail.com>
2025-05-13 16:32:20 +0900

    [add]EnemyManagerの追加

---
+++ Assets/Script/System/Ingame/Enemy/EnemyManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    List<IEnemy> _enemies = new();
+    public List<IEnemy> Enemies => _enemies;
+    public bool IsEnemyAllDefeated => _enemies.Count == 0;
+    void Start()
+    {
+        _enemies = FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None).OfType<IEnemy>().ToList();
+    }
+
+    void Update()
+    {
+
+    }
+}
