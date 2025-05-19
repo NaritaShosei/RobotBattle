009dbb04afcddac1d5e3d6af4098af4b744e1a40
NaritaShosei <onigirikun7171@gmail.com>
2025-05-19 16:49:20 +0900

    [add]Animation制御クラス

---
+++ Assets/Script/System/Ingame/AnimationController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField]
+    Animator _animator;
+
+    public void SetFloat(string name, float value)
+    {
+        _animator.SetFloat(name, value);
+    }
+
+    public void SetBool(string name, bool value)
+    {
+        _animator.SetBool(name, value);
+    }
+
+    public void SetInteger(string name, int value)
+    {
+        _animator.SetInteger(name, value);
+    }
+
+    public void SetTrigger(string name)
+    {
+        _animator.SetTrigger(name);
+    }
+
+    public void PlayDead()
+    {
+        foreach (AnimatorControllerParameter param in _animator.parameters)
+        {
+            if (param.type == AnimatorControllerParameterType.Trigger)
+            {
+                _animator.ResetTrigger(param.name);
+            }
+
+            if (param.type == AnimatorControllerParameterType.Bool)
+            {
+                _animator.SetBool(_animator.name, false);
+            }
+        }
+        // _animator.SetBool("死", true);
+    }
+}
