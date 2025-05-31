3fd3c5bcc0dddcd9dae01f23e1bd7140b7514c86
NaritaShosei <onigirikun7171@gmail.com>
2025-05-31 20:36:50 +0900

    [add]ServiceLocatorの追加

---
+++ Assets/Script/System/ServiceLocator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceLocator : MonoBehaviour
+{
+    public static Dictionary<Type, object> _service = new();
+
+    public static void Set<T>(T service)
+    {
+        _service.Add(typeof(T), service);
+    }
+
+    public static T Get<T>()
+    {
+        return (T)_service[typeof(T)];
+    }
+}
