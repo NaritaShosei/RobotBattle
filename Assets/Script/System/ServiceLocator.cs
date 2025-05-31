bee65fdef8e775df63554fbf5fe4e6557aa97219
NaritaShosei <onigirikun7171@gmail.com>
2025-06-01 01:10:35 +0900

    [update]登録済みであれば上書きする処理

--- Assets/Script/System/ServiceLocator.cs
+++ Assets/Script/System/ServiceLocator.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ServiceLocator : MonoBehaviour
+public static class ServiceLocator
 {
     public static Dictionary<Type, object> _service = new();
 
     public static void Set<T>(T service)
     {
+        if (_service.ContainsKey(typeof(T)))
+        {
+            _service[typeof(T)] = service;
+            return;
+        }
         _service.Add(typeof(T), service);
     }
 
