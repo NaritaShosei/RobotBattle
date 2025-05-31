1459663c430037dd6bc204d52c966c19d52accea
NaritaShosei <onigirikun7171@gmail.com>
2025-06-01 01:22:16 +0900

    [add]AudioManagerの作成

---
+++ Assets/Script/System/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource _audio;
+    void Start()
+    {
+        if (_audio == null)
+        {
+            _audio = GetComponent<AudioSource>();
+        }
+        ServiceLocator.Set(this);
+    }
+
+    public void PlayAudio(AudioData data)
+    {
+        _audio.PlayOneShot(data.AudioClip, data.Volume);
+    }
+}
