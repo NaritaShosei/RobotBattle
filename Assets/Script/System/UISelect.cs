e5f6ecf9a5acf923df776f81ce3c99258a0e6e09
NaritaShosei <onigirikun7171@gmail.com>
2025-06-07 20:22:59 +0900

    [add]まずはDebug用だがUI選択のもの

---
+++ Assets/Script/System/UISelect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class UISelect : MonoBehaviour
+{
+    [SerializeField] Image[] _selects;
+    int _selectIndex;
+    InputManager _input;
+    private void Awake()
+    {
+        _input = ServiceLocator.Get<InputManager>();
+        _input.UISelectAction.performed += Select;
+    }
+
+    void Select(InputAction.CallbackContext context)
+    {
+        Debug.LogError("SELECT");
+        _input.SwitchInputMode(ModeType.Player);
+        ServiceLocator.Get<InGameManager>().PauseResume();
+    }
+    private void OnDisable()
+    {
+        _input.UISelectAction.performed -= Select;
+    }
+}
