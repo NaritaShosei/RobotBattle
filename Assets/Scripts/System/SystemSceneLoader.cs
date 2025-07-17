7887806f21e33a428a5e407426deeb0353f14ff6
NaritaShosei <onigirikun7171@gmail.com>
2025-07-17 21:33:43 +0900

    [add]SystemScene

---
+++ Assets/Scripts/System/SystemSceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SystemSceneLoader : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void Initialize()
+    {
+        if (!SceneManager.GetSceneByName("SystemScene").isLoaded)
+        {
+            SceneManager.LoadScene("SystemScene", LoadSceneMode.Additive);
+        }
+    }
+}
