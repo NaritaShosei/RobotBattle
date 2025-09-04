85e1cbdd8cd62a3c674ad0b9cc904d90d85ddabe
NaritaShosei <onigirikun7171@gmail.com>
2025-09-04 22:56:21 +0900

    [add]アウトゲームの武器システムで使うクラス

---
+++ Assets/Scripts/System/Ingame/Data/WeaponData/WeaponData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "WeaponData", fileName = "WeaponData")]
+public class WeaponData : ScriptableObject
+{
+    [Header("基本情報")]
+    [SerializeField] private int _weaponID;
+    [SerializeField] private string _weaponName;
+    [SerializeField] private Sprite _weaponIcon;
+    [SerializeField] private int _weaponMoney;
+
+    [Header("性能")]
+    [SerializeField] private float _attackPower = 1;
+    [SerializeField] private float _attackRate = 1;
+    [SerializeField] private float _range = 10;
+
+    [Header("プレハブ")]
+    [SerializeField] private GameObject _weaponPrefab;
+    public int WeaponID => _weaponID;
+    public string WeaponName => _weaponName;
+    public Sprite WeaponIcon => _weaponIcon;
+    public int WeaponMoney => _weaponMoney;
+    public float AttackPower => _attackPower;
+    public float AttackRate => _attackRate;
+    public float Range => _range;
+    public GameObject WeaponPrefab => _weaponPrefab;
+
+}
---
+++ Assets/Scripts/System/WeaponManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WeaponManager : MonoBehaviour, IPointerClickHandler
+{
+    public LongRangeAttack_B MainWeapon { get; private set; }
+    public LongRangeAttack_B SubWeapon { get; private set; } 
+    private void Awake()
+    {
+        ServiceLocator.Set(this);
+    }
+
+    public void SetMainWeapon(WeaponCellView view)
+    {
+        //MainWeapon = view.Weapon;
+    }
+    public void SetSubWeapon(WeaponCellView view)
+    {
+        //SubWeapon = view.Weapon;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var obj = eventData.pointerCurrentRaycast.gameObject;
+
+        if (obj.TryGetComponent(out WeaponCellView view))
+        {
+
+        }
+    }
+}
