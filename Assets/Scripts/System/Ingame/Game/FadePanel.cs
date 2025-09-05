596acf121209131b0eb87f62a37e296a29f04441
NaritaShosei <onigirikun7171@gmail.com>
2025-09-05 15:12:58 +0900

    [update]Fade

---
+++ Assets/Scripts/System/Ingame/Game/FadePanel.cs
@@ -0,0 +1,21 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadePanel : MonoBehaviour
+{
+    [SerializeField] private Image _fadePanel;
+    [SerializeField] private float _duration = 0.5f;
+
+    private void Awake()
+    {
+        ServiceLocator.Set(this);
+    }
+
+    public async UniTask Fade(float alpha)
+    {
+        await _fadePanel.DOFade(alpha, _duration).AsyncWaitForCompletion();
+    }
+}
