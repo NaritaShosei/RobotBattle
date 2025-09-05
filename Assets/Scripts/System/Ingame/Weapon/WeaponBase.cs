a6dc1d6ca686fea0aad130b78daf51b58c2fdcfe
NaritaShosei <onigirikun7171@gmail.com>
2025-09-05 18:03:56 +0900

    [update]武器の基底クラス

---
+++ Assets/Scripts/System/Ingame/Weapon/WeaponBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public abstract class WeaponBase : MonoBehaviour
+{
+    protected WeaponData _data;
+
+    public WeaponData Data => _data;
+
+    public virtual void Initialize(WeaponData data)
+    {
+        _data = data;
+        OnInitialize();
+    }
+
+    protected virtual void OnInitialize() { }
+    public abstract void Attack();
+}
