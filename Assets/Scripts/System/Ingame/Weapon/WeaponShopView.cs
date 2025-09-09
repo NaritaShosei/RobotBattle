cdd3626d94f81a2c8cf768c2b9af25232c6ca8b3
NaritaShosei <onigirikun7171@gmail.com>
2025-09-09 20:27:33 +0900

    [add]購入のUI、購入はまだできない

---
+++ Assets/Scripts/System/Ingame/Weapon/WeaponShopView.cs
@@ -0,0 +1,79 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WeaponShopView : MonoBehaviour, IPointerClickHandler
+{
+    private WeaponDatabase _weaponDatabase;
+    [SerializeField] private WeaponCell _weaponCell;
+    [SerializeField] private Transform _cellParent;
+    [SerializeField] private WeaponExplanation _explanation;
+    private WeaponCell _currentCell;
+    private List<WeaponCell> _cells = new();
+    [Header("アニメーション設定")]
+    [SerializeField] private float _animationDuration = 0.2f;
+    private WeaponSelector _selector;
+
+    private void Start()
+    {
+        _weaponDatabase = ServiceLocator.Get<WeaponManager>().DataBase;
+        _selector = ServiceLocator.Get<WeaponSelector>();
+        SetUI();
+    }
+
+    private void SetUI()
+    {
+        int[] unlockedIds = _selector.GetUnlockIDs().ToArray();
+        int[] allIds =_weaponDatabase.GetAllWeapons().Select(d => d.WeaponID).ToArray();
+
+        int[] lockedIds = allIds.Except(unlockedIds).ToArray();
+
+        foreach (var id in lockedIds)
+        {
+            var data = _weaponDatabase.GetWeapon(id);
+
+            var cell = Instantiate(_weaponCell, _cellParent);
+            cell.Initialize(data.WeaponIcon, "$", data.WeaponMoney, data.WeaponID);
+            _cells.Add(cell);
+        }
+
+        _currentCell = _cells[0];
+        _currentCell.Select();
+        SetExplanation(_currentCell.Id);
+    }
+
+    private void SetExplanation(int id)
+    {
+        var data = _weaponDatabase.GetWeapon(id);
+
+        if (data != null)
+            _explanation.Set(data.WeaponName, data.AttackPower, data.AttackRate);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+        if (eventData.pointerCurrentRaycast.gameObject.TryGetComponent(out WeaponCell cell))
+        {
+            Debug.Log(cell.gameObject.name);
+
+            _currentCell.UnSelect();
+
+            _currentCell = cell;
+            _currentCell.Select();
+
+            SetExplanation(_currentCell.Id);
+        }
+    }
+    /// <summary>
+    /// ボタンクリック時のアニメーション
+    /// </summary>
+    public void OnClick(int target)
+    {
+        transform.DOScaleY(target, _animationDuration).
+            SetEase(Ease.OutElastic);
+    }
+}
