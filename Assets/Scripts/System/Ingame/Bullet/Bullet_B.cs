d7c4980083e988068b890c6ad482066a914fb0f3
NaritaShosei <onigirikun7171@gmail.com>
2025-09-13 18:12:56 +0900

    [update]弾が移動距離に応じて消えるように

--- Assets/Scripts/System/Ingame/Bullet/Bullet_B.cs
+++ Assets/Scripts/System/Ingame/Bullet/Bullet_B.cs
@@ -1,21 +1,24 @@
-﻿using Script.System.Ingame;
-using System;
+﻿using Cysharp.Threading.Tasks;
 using UnityEngine;
+using System.Threading;
 
-public abstract class Bullet_B : MonoBehaviour
+public abstract class Bullet_B : MonoBehaviour, IWeapon
 {
     [SerializeField] protected float _enableTime = 1.5f;
     protected ILockOnTarget _target;
-    public Action ReturnPoolEvent;
+    public System.Action ReturnPoolEvent;
     protected float _timer;
 
     protected bool _isTimeReturned;
     protected bool _isConflictReturned;
     public float GuardBreakValue => _weaponData.GuardBreakValue;
-
     public float AttackPower => _weaponData.AttackPower;
 
     protected WeaponData _weaponData;
+
+    // 発射位置を記憶
+    private Vector3 _startPos;
+    private CancellationTokenSource _cts;
 
     public virtual void Initialize(WeaponData data)
     {
@@ -23,28 +26,51 @@ public abstract class Bullet_B : MonoBehaviour
     }
 
     private void OnEnable() => OnEnable_B();
-
     private void OnDisable() => OnDisable_B();
 
     protected virtual void OnEnable_B()
     {
         _isTimeReturned = false;
         _isConflictReturned = false;
+        _startPos = transform.position;
+
+        // キャンセルトークン更新
+        _cts = new CancellationTokenSource();
+        MoveAsync(_cts.Token).Forget();
     }
     protected virtual void OnDisable_B()
     {
+        _cts?.Cancel();
         if (_isTimeReturned || _isConflictReturned)
         {
             ReturnPoolEvent?.Invoke();
         }
     }
+
+    async UniTaskVoid MoveAsync(CancellationToken token)
+    {
+        while (!_isTimeReturned && !_isConflictReturned && !token.IsCancellationRequested)
+        {
+            float speed = _weaponData.AttackSpeed;
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+            // 移動距離チェック
+            if (Vector3.Distance(_startPos, transform.position) >= _weaponData.Range)
+            {
+                _isTimeReturned = true;
+                gameObject.SetActive(false); // プールに返す
+                break;
+            }
+
+            await UniTask.Yield(PlayerLoopTiming.Update, token);
+        }
+    }
+
     void Update()
     {
         OnUpdate();
     }
-    /// <summary>
-    /// Updateで実行したい処理をここに書く
-    /// </summary>
+
     abstract protected void OnUpdate();
 
     void OnTriggerEnter(Collider other)
@@ -52,11 +78,9 @@ public abstract class Bullet_B : MonoBehaviour
         if (other.CompareTag("IgnoreCollider")) return;
         Conflict(other);
     }
-    /// <summary>
-    /// ぶつかった時の処理
-    /// </summary>
-    /// <param name="other"></param>
+
     protected abstract void Conflict(Collider other);
+
     public virtual void SetTarget(ILockOnTarget target)
     {
         _target = target;
@@ -65,9 +89,13 @@ public abstract class Bullet_B : MonoBehaviour
             transform.forward = (target.GetTargetCenter().position - transform.position).normalized;
         }
     }
+
     public virtual void SetPosition(Vector3 pos)
     {
         _timer = 0;
         transform.position = pos;
+        _startPos = pos;
     }
+
+    public float GetAttackPower() => _weaponData.AttackPower;
 }
