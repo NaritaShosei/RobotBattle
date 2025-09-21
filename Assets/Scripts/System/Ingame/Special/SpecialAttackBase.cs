98470fcca6fccbcb082a6e704c68af40a850e668
NaritaShosei <onigirikun7171@gmail.com>
2025-09-22 05:13:22 +0900

    [update]必殺技基底クラスをabstractに。必殺技を待てるようにした

--- Assets/Scripts/System/Ingame/Special/SpecialAttackBase.cs
+++ Assets/Scripts/System/Ingame/Special/SpecialAttackBase.cs
@@ -1,16 +1,24 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 
-public class SpecialAttackBase : MonoBehaviour
+public abstract class SpecialAttackBase : MonoBehaviour, IWeapon
 {
+    private SpecialData _data;
+    public SpecialData Data => _data;
 
-    private void Start()
+    public float GetAttackPower()
     {
+        return Data.AttackPower;
+    }
 
+    public void Initialize(SpecialData data)
+    {
+        _data = data;
     }
 
     /// <summary>
     /// 必殺技の発動
     /// </summary>
-    public void SpecialAttack() { }
+    public abstract UniTask SpecialAttack();
 
 }
