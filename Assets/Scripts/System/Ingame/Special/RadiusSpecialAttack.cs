b1f8a6102d88ee52d08d51be8135d10500eb771d
NaritaShosei <onigirikun7171@gmail.com>
2025-09-22 05:27:10 +0900

    [add]自身を中心とした範囲必殺技の作成。現状毎フレーム攻撃しているので別の方法にする予定。

---
+++ Assets/Scripts/System/Ingame/Special/RadiusSpecialAttack.cs
@@ -0,0 +1,41 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class RadiusSpecialAttack : SpecialAttackBase
+{
+    [ContextMenu("範囲必殺技")]
+    public override async UniTask SpecialAttack()
+    {
+        float timer = 0;
+        try
+        {
+            while (timer < Data.Duration)
+            {
+                // この必殺技は一旦Playerの中心に存在すること前提として作る
+                var colliders = Physics.OverlapSphere(transform.position, Data.Range * 0.5f);
+
+                foreach (var collider in colliders)
+                {
+                    if (collider.TryGetComponent(out IEnemySource enemy))
+                    {
+                        enemy.HitDamage(this);
+                        Debug.LogError(enemy.GetTargetCenter().gameObject.name);
+                    }
+                }
+
+                timer += Time.deltaTime;
+                await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken: destroyCancellationToken);
+            }
+        }
+        catch { }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, Data.Range * 0.5f);
+        }
+    }
+}
