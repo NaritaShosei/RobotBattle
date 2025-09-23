afe9383e8991d65b997419ed064b4ed71a19dc52
NaritaShosei <onigirikun7171@gmail.com>
2025-09-23 23:00:44 +0900

    [update]装備場所を決めるクラス

---
+++ Assets/Scripts/System/Ingame/Player/PlayerEquipmentManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEquipmentManager : MonoBehaviour
+{
+    [SerializeField] private Transform _left;
+    [SerializeField] private Transform _right;
+    [SerializeField] private Transform _center;
+
+    private Dictionary<EquipmentType, Transform> _parents;
+
+    private void Awake()
+    {
+        _parents = new Dictionary<EquipmentType, Transform>
+        {
+            { EquipmentType.Left, _left },
+            { EquipmentType.Right, _right },
+            { EquipmentType.Center, _center }
+        };
+    }
+
+    public Transform GetEquipmentParent(EquipmentType type)
+    {
+        return _parents[type];
+    }
+}
+
+public enum EquipmentType
+{
+    Left,
+    Right,
+    Center,
+}
