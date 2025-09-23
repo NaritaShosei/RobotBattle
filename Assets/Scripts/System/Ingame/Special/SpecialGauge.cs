0b21f2d9df807e43c72b6fb30a49a4c45e32d00f
NaritaShosei <onigirikun7171@gmail.com>
2025-09-23 21:35:56 +0900

    [update]必殺ゲージのロジック

---
+++ Assets/Scripts/System/Ingame/Special/SpecialGauge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class SpecialGauge
+{
+    private float _value;
+    private float _max = 100;
+    public event Action<float> OnValueChange;
+    public event Action OnUseGauge;
+    public event Action OnGaugeMax;
+    public bool IsGaugeMax => _value >= _max;
+
+    public void Initialize(float max)
+    {
+        _max = max;
+    }
+
+    public void UpdateValue(float value)
+    {
+        if (IsGaugeMax) { return; }
+
+        var oldValue = _value;
+
+        _value = Mathf.Min(value, _max);
+        OnValueChange?.Invoke(_value / _max);
+
+        if (IsGaugeMax)
+        {
+            OnGaugeMax?.Invoke();
+        }
+    }
+
+    public bool TryUseGauge()
+    {
+        if (!IsGaugeMax) { return false; }
+
+        OnUseGauge?.Invoke();
+        _value = 0;
+
+        return true;
+    }
+}
