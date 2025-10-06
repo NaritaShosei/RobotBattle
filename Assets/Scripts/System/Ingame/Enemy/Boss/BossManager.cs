bc13084e5b2b881860bfd08f7d4e9715e7386ccb
NaritaShosei <onigirikun7171@gmail.com>
2025-10-06 16:12:34 +0900

    [add]適当なボスの追加

---
+++ Assets/Scripts/System/Ingame/Enemy/Boss/BossManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    [SerializeField] private BossEnemy _boss;
+    [SerializeField] private Transform _spawn;
+
+    public void SpawnBoss()
+    {
+        Instantiate(_spawn, _spawn.position, Quaternion.identity);
+    }
+}
