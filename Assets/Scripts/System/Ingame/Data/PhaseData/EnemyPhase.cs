7b159c114e4e1105a7dfefeb1bb20c490025b274
NaritaShosei <onigirikun7171@gmail.com>
2025-10-06 18:41:24 +0900

    [add]敵全滅が条件のフェーズ

---
+++ Assets/Scripts/System/Ingame/Data/PhaseData/EnemyPhase.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyPhase", menuName = "GameData/PhaseData/EnemyPhase")]
+public class EnemyPhase : PhaseData_B
+{
+    public override async UniTask Run(PhaseContext context)
+    {
+        var em = ServiceLocator.Get<EnemyManager>();
+
+        await em.WaitUntilGameResumed();
+
+        em.StartSpawnersAsync().Forget();
+
+        await UniTask.WaitUntil(() => em.IsEnemyAllDefeated);
+    }
+}
