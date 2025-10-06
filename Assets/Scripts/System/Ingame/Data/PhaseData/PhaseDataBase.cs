a5a7a1d211b1b64e3c13f0f6585bf7f183c4a094
NaritaShosei <onigirikun7171@gmail.com>
2025-10-06 18:42:08 +0900

    [add]フェーズの流れを保持するデータベース

---
+++ Assets/Scripts/System/Ingame/Data/PhaseData/PhaseDataBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PhaseDataBase", menuName = "GameData/PhaseDataBase")]
+public class PhaseDataBase : ScriptableObject
+{
+    [SerializeField] private List<PhaseData_B> _phases;
+
+    public List<PhaseData_B> AllPhaseData => _phases;
+}
