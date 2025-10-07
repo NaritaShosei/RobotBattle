404fa7a8e2cb88f6bfc5c3455853d658683606e5
NaritaShosei <onigirikun7171@gmail.com>
2025-10-07 14:30:53 +0900

    [add]TimeLineの演出終了まで待つフェーズ

---
+++ Assets/Scripts/System/Ingame/Data/PhaseData/TimeLinePhase.cs
@@ -0,0 +1,18 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[CreateAssetMenu(fileName = "TimeLinePhase", menuName = "GameData/PhaseData/TimeLinePhase")]
+public class TimeLinePhase : PhaseData_B
+{
+    [SerializeField] private TimelineAsset _timeLine;
+    public override async UniTask Run(PhaseContext context)
+    {
+        var pd = FindAnyObjectByType<PlayableDirector>();
+
+        pd.Play(_timeLine);
+
+        await UniTask.WaitUntil(() => pd.state != PlayState.Playing);
+    }
+}
