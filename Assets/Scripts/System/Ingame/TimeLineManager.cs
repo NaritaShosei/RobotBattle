55c321ffd201d36cd68848a839d29268905faaa8
NaritaShosei <onigirikun7171@gmail.com>
2025-10-07 14:39:28 +0900

    [add]TimeLine管理クラス

---
+++ Assets/Scripts/System/Ingame/TimeLineManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimeLineManager : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector _playableDirector;
+    public PlayableDirector PlayableDirector => _playableDirector;
+
+    private void Awake()
+    {
+        ServiceLocator.Set(this);
+    }
+}
